32db7cf78a7598d9de55d04bdbefaa576b0f3850
Vibhor Phalke <vibgitcode27@gmail.com>
2024-12-12 18:11:40 +0530

    added antD , redux@toolkit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import { ConfigProvider } from "antd";
+import StoreProvider from "./StoreProvider";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -24,10 +26,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+        <ConfigProvider>
+          <StoreProvider>
+            {children}
+          </StoreProvider>
+        </ConfigProvider>
       </body>
     </html>
   );
