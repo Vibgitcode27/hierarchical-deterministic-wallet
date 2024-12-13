e9d4a99054b123a0e0cabeb9501ab2bb3b4ddfc2
Vibhor Phalke <vibgitcode27@gmail.com>
2024-12-14 01:45:35 +0530

    added /home 2/4 section

--- src/app/(pages)/home/page.tsx
+++ src/app/(pages)/home/page.tsx
@@ -1,10 +1,48 @@
-import { Flex , Image } from "antd";
+"use client"
+
+import { Flex , Image , Button , Avatar } from "antd";
+import { useRouter } from "next/navigation";
+import { WalletOutlined } from '@ant-design/icons';
 import guy1 from "../../assets/image-removebg-preview (2).png"
 import guy2 from "../../assets/image-removebg-preview (1) (1).png"
-import logo from "../../assets/profile-removebg-preview.png"
 import "../../styles/button.css"
 import "../../styles/home.css"
+
 export default function Home() {
+
+  const navigate = useRouter();
+
+  const accountData = [
+    {
+      id: '1',
+      serial: 1,
+      avatar: 'https://api.dicebear.com/7.x/miniavars/svg?seed=Bitcoin',
+      accountName: 'Bitcoin Wallet',
+      accountHash: '0x1234...5678',
+    },
+    {
+      id: '2',
+      serial: 2,
+      avatar: 'https://api.dicebear.com/7.x/miniavars/svg?seed=Ethereum',
+      accountName: 'Ethereum Wallet',
+      accountHash: '0x9876...5432',
+    },
+    {
+      id: '3',
+      serial: 3,
+      avatar: 'https://api.dicebear.com/7.x/miniavars/svg?seed=USDC',
+      accountName: 'USDC Wallet',
+      accountHash: '0xABCD...EFGH',
+ 
+    },{
+      id: '4',
+      serial: 4,
+      avatar: 'https://api.dicebear.com/7.x/miniavars/svg?seed=USDC',
+      accountName: 'XTZ Wallet',
+      accountHash: '0xABCD...EFGH',
+ 
+    }
+  ];
 
  return(
     <div style={{ 
@@ -95,16 +133,74 @@ export default function Home() {
           </Flex>
         </Flex>
         <Flex 
+          vertical
           style={{ 
             color: "white", 
             backgroundColor: "#1f2023", 
             width: "30vw", 
             height: "42vh",
             borderRadius: "15px",
-            padding: "20px"
+            padding: "20px",
           }}
         >
-          <h1>ACCOUNTS</h1>
+          <h1 style={{ color: 'white', marginBottom: '20px' }}>ACCOUNTS</h1>
+          
+          <Flex vertical gap="middle"   style={{ overflowY: 'auto', scrollbarWidth: 'none', msOverflowStyle: 'none',}}>
+            {accountData.map((account) => (
+              <Flex 
+                key={account.id} 
+                justify="space-between" 
+                align="center"
+                style={{
+                  borderRadius: '10px'
+                }}
+              >
+                <Flex align="center" gap="middle">
+                  <Avatar
+                    size={55} 
+                    icon={<WalletOutlined />} 
+                  />
+                  <Flex vertical>
+                    <span style={{ color: 'white', fontWeight: 'bold' , fontSize : "25px" , lineHeight : "45px" }}>
+                      {account.accountName}
+                    </span>
+                    <small style={{ color: '#888' , fontSize : "19px" }}>
+                      {account.accountHash}
+                    </small>
+                  </Flex>
+                </Flex>
+
+                {/* <Flex align="center" justify="center" style={{ height : "100%" }}>
+                  <Button 
+                    type="primary" 
+                    icon={<SwapOutlined />} 
+                    onClick={() => navigate.push(`/transactions/swap/${account.id}`)}
+                    style={{ width: '100%' }}
+                  >
+                    Swap
+                  </Button>
+                </Flex> */}
+                <Flex vertical align="center" justify="center" style={{ height : "100px"}}>
+                  <Flex
+                    style={{
+                      borderRadius: "30px", 
+                      border: "none",
+                      backgroundColor: "#1c49ff",
+                      color: "black",
+                      paddingInline: "10px",
+                      paddingBlock: "3px",
+                      fontSize: "18px",
+                      fontFamily: "sans-serif",
+                      fontWeight: "600",
+                      height : "30px",
+                      cursor : "pointer"
+                    }}>
+                      INTERACT
+                  </Flex>
+                </Flex>
+            </Flex>
+            ))}
+          </Flex>
         </Flex>
       </Flex>
     </div>
